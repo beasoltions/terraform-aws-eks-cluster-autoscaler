5cb67324d8546c428e27b25931c7fa00f9323548
Deen Muhammad <40787426+deenMuhammad@users.noreply.github.com>
2024-01-25 00:50:24 +0900

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,7 +1,7 @@
 data "kubernetes_all_namespaces" "allns" {}
 
 resource "kubernetes_namespace" "cluster_autoscaler" {
-  count = (contains(data.kubernetes_all_namespaces.allns.namespaces, var.k8s_namespace)) ? 0 : 1
+  count = (contains(data.kubernetes_all_namespaces.allns.namespaces, var.namespace)) ? 0 : 1
   metadata {
     name = var.namespace
   }
