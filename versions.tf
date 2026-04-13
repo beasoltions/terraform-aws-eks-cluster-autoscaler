058de17290b45cd16a3fbfad819e80cffb01efcc
Deen Muhammad <40787426+deenMuhammad@users.noreply.github.com>
2026-04-13 23:32:45 +0900

    Update AWS provider version constraint

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13"
 
   required_providers {
-    aws        = "~> 5.40.0"
+    aws = ">= 5.40.0, < 6.0.0"
     helm       = ">= 1.0, < 3.1"
     kubernetes = ">= 1.10.0, < 3.0.0"
   }
